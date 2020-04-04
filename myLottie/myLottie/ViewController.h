0b80ec81d0722d978ec7b1749c689d939e228a7d
guokai <guokai@guokaideMacBook-Air.local>
2020-04-04 18:56:12 +0800

    creat by guo

---
+++ myLottie/myLottie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myLottie
+//
+//  Created by guokai on 2020/4/4.
+//  Copyright © 2020 guokai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ myLottie/myLottie/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  myLottie
+//
+//  Created by guokai on 2020/4/4.
+//  Copyright © 2020 guokai. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
